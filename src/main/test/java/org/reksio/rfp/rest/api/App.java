64bfef3f3a7eb2d457626320c288629fae8c5006
Spuu <m.ruszewski@gmail.com>
2017-05-05 00:31:39 +0200

    Added product search

--- src/main/test/java/org/reksio/rfp/rest/api/App.java
+++ src/main/test/java/org/reksio/rfp/rest/api/App.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Random;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringAppRepoConfig.class)
 public class App {
@@ -57,4 +59,38 @@ public class App {
         peopleRepository.save(ludek2);*/
     }
 
+    String generateNumber(int digits) {
+        if (digits <= 0)
+            return "0";
+
+        Random rnd = new Random();
+        String number = "";
+
+        for(int i=0;i<digits;++i) {
+            number += String.valueOf(rnd.nextInt(10));
+        }
+
+        return number;
+    }
+
+    @Test
+    public void generateProducts() {
+
+        String[] firstNames = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+        String[] lastNames = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+        int length = 100;
+
+        productRepository.deleteAll();
+
+        Random rnd = new Random();
+
+        for (int i=0;i<length;++i) {
+            Product product = Product.builder()
+                    .name(String.join(" ", firstNames[rnd.nextInt(firstNames.length)], lastNames[rnd.nextInt(lastNames.length)]))
+                    .ean(generateNumber(13))
+                    .build();
+
+            productRepository.save(product);
+        }
+    }
 }
